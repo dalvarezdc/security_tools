4af2dc6d825f6ef6b9495a06e4caeb994e3c757a
Administrator <daniel.ad0791@gmail.com>
2024-05-08 06:47:38 +0200

    Update in ip details

--- vpn_leak_detector/get_ip_details.sh
+++ vpn_leak_detector/get_ip_details.sh
@@ -9,6 +9,7 @@ country=$(echo "$ip_info" | grep '"country":' | sed 's/.*: "\(.*\)",/\1/')
 region=$(echo "$ip_info" | grep '"region":' | sed 's/.*: "\(.*\)",/\1/')
 city=$(echo "$ip_info" | grep '"city":' | sed 's/.*: "\(.*\)",/\1/')
 org=$(echo "$ip_info" | grep '"org":' | sed 's/.*: "\(.*\)",/\1/')
+loc=$(echo "$ip_info" | grep '"loc":' | sed 's/.*: "\(.*\)",/\1/')
 
 # Print the results
 echo "IP Address: $ip_address"
@@ -16,3 +17,6 @@ echo "Country: $country"
 echo "Region: $region"
 echo "City: $city"
 echo "Organization: $org"
+echo "Location: $loc"
+
+#  echo "$ip_info"
