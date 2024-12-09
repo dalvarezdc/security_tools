f3f9a50f5024139e2977a4c434b28b3de43775c8
Administrator <daniel.ad0791@gmail.com>
2024-12-10 03:32:39 +0400

    Improved websites to make analysis

--- browser_utilities/crypto_trading_open.sh
+++ browser_utilities/crypto_trading_open.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-https://jup.ag/
 
 CHROME_PROFILE="Profile 2"
 BROWSER_EXEC="google-chrome"
@@ -7,22 +6,27 @@ BROWSER_EXEC="google-chrome"
 FIREFOX_WEBSITES=(
   "https://dexscreener.com/?rankBy=trendingScoreH1&order=desc&chainIds=solana&minLiq=10000&minMarketCap=65000&maxMarketCap=11000000&min1HTxns=200&min1HVol=8000"
   "https://gmgn.ai/?chain=sol"
-  "https://solanabox.tools/"
-  "https://rugcheck.xyz/"
+  "https://app.cielo.finance/feed"
 )
 CHROME_WEBSITES=(
-  "https://www.drift.trade/"
   "https://photon-sol.tinyastro.io/en/discover"
-  "https://www.sniper.xyz/"
+  "https://jup.ag/"
+  "https://rugcheck.xyz/"
   "https://www.solsniffer.com/"
+  "https://app.tweetscout.io/"
 )
 
 # Declare an associative array
 declare -A my_array=(
     [orangie_twitter]="https://x.com/nftimm_sol/status/1738497599873880575"
-    [orangie1]="https://www.potionalpha.com/"
+    [orangie_dicord_bot_expensive]="https://www.potionalpha.com/"
     [orangie_wallet_tracker]="https://app.cielo.finance/feed"
     [trenchy_bot]="https://t.me/TrenchyBot"
+    [solana_tools]="https://solanabox.tools/"
+    [perps_trading]="https://www.drift.trade/"
+    [sniper_xyz]="https://www.sniper.xyz/"
+    [solana_trading_bot_1]="https://www.solanatracker.io/solana-trading-bot"
+    [twitter_check]="https://app.tweetscout.io/"
 )
 
 # Iterate over the array and print key-value pairs
